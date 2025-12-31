6a9844886883c26e0bd61392c75faa645ce98e78
Vincentchirchir <108663966+Vincentchirchir@users.noreply.github.com>
2025-12-30 21:15:22 -0700

    Update urls.py

--- tambulyouthgroup_portal/urls.py
+++ tambulyouthgroup_portal/urls.py
@@ -20,6 +20,17 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+#urlpatterns = [
+  #  path('admin/', admin.site.urls),
+   # path('', include('core.urls')),
+  #  path('accounts/', include('accounts.urls')),
+  #  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+  #  path("accounts/", include("django.contrib.auth.urls"))
+#]
+
+#if settings.DEBUG:
+   # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
@@ -28,5 +39,5 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls"))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
